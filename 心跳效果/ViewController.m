d18647525c6ee3f67a9e4bf9b11641ae47fe2f9b
liubinpeng <1611274445@qq.com>
2018-01-11 08:47:20 +0800

    add

--- 心跳效果/ViewController.m
+++ 心跳效果/ViewController.m
@@ -20,7 +20,7 @@
     CABasicAnimation *animation = [CABasicAnimation animation];
     animation.toValue = @0.2;
     animation.keyPath = @"transform.scale";
-    animation.repeatCount = MAXFLOAT;
+    animation.repeatCount = HUGE_VALF;
     animation.duration = 0.5;
     animation.autoreverses = YES;
     [self.imageView.layer addAnimation:animation forKey:nil];
